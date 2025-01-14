54cb567f3d3d568fc4a83bd6555329e7f668ddba
NMT2211 <nguyentuanw135@gmail.com>
2025-01-14 21:14:58 +0700

    goc

---
+++ ASM_Java5/src/main/java/com/test/test.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class test {
+
+	@GetMapping("/test")
+	public String test() {
+		return "view/test";
+	}
+	
+	
+}
